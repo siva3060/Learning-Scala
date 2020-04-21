8709eb3b4a0d07d4dbc36224d3db700301b23e36
siva <siva3060@gmail.com>
2020-04-21 08:49:03 +0530

    ch2 ex4 : conside code implementation

---
+++ chapter2/ch2ex4.scala
@@ -0,0 +1,19 @@
+
+/**
+ * Description : writing the code more consise
+ * Author : Siva Charan
+ * email : siva3060@gamil.com
+ *
+ */
+
+val flag : Boolean = false
+val result : Boolean = (flag == false)
+
+// 1.we will use type inference in scala to take of the types 
+// 2.we can use the value of flag/newflag  using the NOT operator
+
+val newflag = false
+val newresult = (!flag)
+
+println(newresult)
+
