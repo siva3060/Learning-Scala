20e429dfdcc6a8b8b306f366c6bbc24ab35b6df9
siva <siva3060@gmail.com>
2020-04-21 16:21:52 +0530

    Implemented solutions for 3 and 4

---
+++ chapter2/ch2ex3.scala
@@ -0,0 +1,8 @@
+
+//Description : Centigrade to Fahrenheit Conversion
+// save each step of conversion and expect the values 
+//Author :  Siva Charan
+//email : siva3060@gmail.com
+
+val du : Double  = 2.7255
+println(f"You owe $$${du}%.2f")
